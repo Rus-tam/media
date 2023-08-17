d9c7c5c69106a233cfb995583e15207dc05cad87
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-08-17 15:57:18 +0500

    wrapping up the thunk

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,14 @@
+import { fetchUsers } from "../store";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+
 const UsersList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return <div>UsersList</div>;
 };
 
