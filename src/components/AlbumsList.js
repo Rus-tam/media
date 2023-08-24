438a8f4bc069193c1546ea28d69faf200fe1744b
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-08-24 17:01:53 +0500

    remove implementation

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -2,6 +2,7 @@ import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
 import Skeleton from "./Skeleton";
 import ExpandablePanel from "./ExpandablePanel";
 import Button from "./Button";
+import AlbumsListItem from "./AlbumsListItem";
 
 const AlbumsList = ({ user }) => {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
@@ -13,17 +14,12 @@ const AlbumsList = ({ user }) => {
 
   let content;
   if (isLoading) {
-    content = <Skeleton times={3} />;
+    content = <Skeleton className="h-10 w-full" times={3} />;
   } else if (error) {
     content = <div>Error loading albums</div>;
   } else {
     content = data.map((album) => {
-      const header = <div>{album.title}</div>;
-      return (
-        <ExpandablePanel key={album.id} header={header}>
-          List of photos in the panel
-        </ExpandablePanel>
-      );
+      return <AlbumsListItem key={album.id} album={album} />;
     });
   }
 
---
+++ src/components/AlbumsListItem.js
@@ -0,0 +1,32 @@
+import { GoTrash } from "react-icons/go";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+import { useRemoveAlbumMutation } from "../store";
+
+function AlbumsListItem({ album }) {
+  const [removeAlbum, results] = useRemoveAlbumMutation();
+
+  const handleRemoveAlbum = () => {
+    removeAlbum(album);
+  };
+
+  const header = (
+    <>
+      <Button
+        className="mr-2"
+        loading={results.isLoading}
+        onClick={handleRemoveAlbum}
+      >
+        <GoTrash />
+      </Button>
+      {album.title}
+    </>
+  );
+  return (
+    <ExpandablePanel key={album.id} header={header}>
+      List of photos in the panel
+    </ExpandablePanel>
+  );
+}
+
+export default AlbumsListItem;
